dea57aa89714b8c292d7f3e0c3b23ba512a2eda7
20481A1219 <81548274+20481A1219@users.noreply.github.com>
2022-11-29 15:23:11 +0530

    Update BST.py

--- python/Trees/BST.py
+++ python/Trees/BST.py
@@ -1,4 +1,3 @@
-#creation of node
 class node:
     def __init__(self,data):
         self.data=data
@@ -25,6 +24,37 @@ class node:
                 self.search(root.left,value)
         else:
             print("element is not there")
+    
+    def minValueNode(self,root):
+        current=root
+        while(current.left is not None):
+            current=current.left
+        return current
+        
+    def delete(self,key,root):
+        if root is None:
+            return root
+        if key<root.data:
+            root.left=self.delete(key,root.left)
+        elif key>root.data:
+            root.right=self.delete(key,root.right)
+        else:
+            if root.left is None:
+                temp=root.right
+                root=None
+                return temp
+            elif root.right is None:
+                temp=root.left
+                root=None
+                return temp
+                
+            temp=self.minValueNode(root.right)
+            
+            root.data=temp.data
+            
+            root.right=self.delete(temp.data,root.right)
+            
+        return root    
     def inorder(self,root):
         if root:
             self.inorder(root.left)
@@ -46,7 +76,7 @@ root.insertion(200)
 root.insertion(56)
 root.insertion(280)
 root.insertion(300)
-root.insertion(60)
+root.insertion(70)
 root.insertion(80)
 print("inorder: ")
 root.inorder(root) 
@@ -60,3 +90,11 @@ print()
 root.search(root,80)
 print()
 root.search(root,1000)
+root.delete(200,root)
+root.inorder(root)
+print()
+root.delete(300,root)
+root.inorder(root)
+print()
+root.delete(60,root)
+root.inorder(root)
