42f0cc05536661452c0542fb527ed53b14d52c6e
20481A1219 <81548274+20481A1219@users.noreply.github.com>
2023-01-16 11:26:40 +0530

    Update Reverse-an-array.py

--- python/Recursion/Reverse-an-array.py
+++ python/Recursion/Reverse-an-array.py
@@ -1,4 +1,4 @@
-#reversing array using recursion
+#reversing an array using recursion
 def reverse(l,i):
     if(i>=len(l)-i-1):
         return
@@ -7,3 +7,15 @@ def reverse(l,i):
 l=[1,2,3,4,5,6,7,8,9,10]
 reverse(l,0)
 print(l)
+
+#recursion with different parameters
+def reverse(a,n):
+    i=len(a)-n
+    if i==len(a):
+        return
+    if i<=len(a)-i-1:
+        a[i],a[len(a)-i-1]=a[len(a)-i-1],a[i]
+    reverse(a,n-1)
+a=[1,2,3,4,5,6,7,8,9,11]
+reverse(a,10)
+print(a)
