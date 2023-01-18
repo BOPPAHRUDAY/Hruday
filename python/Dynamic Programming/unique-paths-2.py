776d0a226725035faad6b387d0fa0163bb273777
20481A1219 <81548274+20481A1219@users.noreply.github.com>
2023-01-18 23:49:29 +0530

    Update unique-paths-2.py

--- python/Dynamic Programming/unique-paths-2.py
+++ python/Dynamic Programming/unique-paths-2.py
@@ -8,8 +8,8 @@ class Solution:
                 if obstacleGrid[i][j]==1:
                     dp[i][j]=0 #as there is no way to come to that place
                 else:
-                    if i-1<0:
-                        dp[i][j]=dp[i][j-1]
+                    if i-1<0:                         #this conditions didnt work if both i and j are 0 and so see min path sum conditions for better understanding
+                        dp[i][j]=dp[i][j-1]           #edge cases
                     if j-1<0:
                         dp[i][j]=dp[i-1][j]
                     if i-1>=0 and j-1>=0:    
