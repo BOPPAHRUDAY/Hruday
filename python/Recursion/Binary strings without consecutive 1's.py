4c959f42fc6d87c660228204d61b411a96fe497c
Boppa Sri Satya Sai Hruday <81548274+20481A1219@users.noreply.github.com>
2024-04-02 21:26:51 +0530

    Create Binary strings without consecutive 1's.py

---
+++ python/Recursion/Binary strings without consecutive 1's.py
@@ -0,0 +1,26 @@
+n=int(input())
+
+def give(ans,temp,i):
+    if i==n:
+        print(temp)
+        return 
+    
+    if temp[-1]=='1':
+        give(ans,temp+"0",i+1) #no need to backtrack
+        
+    if temp[-1]=='0':
+        # add "0" and remove to add "1"
+        temp+='0'
+        give(ans,temp,i+1)
+        temp=temp[:-1]
+        
+        # add "1"
+        temp+='1'
+        give(ans,temp,i+1)
+        
+ans=[]
+if n>=1:
+    give(ans,"0",1)
+    give(ans,"1",1)
+else:
+    print([])
