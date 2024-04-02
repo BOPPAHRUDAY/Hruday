1c168dfe1b317dfc0ec9349a822ab90c99e6f709
Boppa Sri Satya Sai Hruday <81548274+20481A1219@users.noreply.github.com>
2024-04-02 21:45:13 +0530

    Update Binary strings without consecutive 1's.py

--- python/Recursion/Binary strings without consecutive 1's.py
+++ python/Recursion/Binary strings without consecutive 1's.py
@@ -24,3 +24,24 @@ if n>=1:
     give(ans,"1",1)
 else:
     print([])
+
+
+# Or we can do this
+
+
+n=int(input())
+def give(ans,temp,i):
+    if i==n:
+        print(temp)
+        return 
+    if temp[-1]=='1':
+        give(ans,temp+"0",i+1) #because it is a string
+    if temp[-1]=='0':
+        give(ans,temp+"0",i+1)
+        give(ans,temp+"1",i+1)
+ans=[]
+if n>=1:
+    give(ans,"0",1)
+    give(ans,"1",1)
+else:
+    print([])
